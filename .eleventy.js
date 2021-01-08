9ed3a03f0a01b32ea933501086e452573468ab34
Tony DiPasquale <anthony.r.dipasquale@gmail.com>
2021-01-08 12:04:42 -0800

    Try writing to assets

--- .eleventy.js
+++ .eleventy.js
@@ -5,20 +5,13 @@ const path = require("path");
 module.exports = config => {
   config.addPassthroughCopy("assets");
 
-  if (!fs.existsSync("_site/assets/css")) {
-    fs.makeDir("_site/assets/css", { recursive: true })
-      .then(renderSass);
-  } else {
-    renderSass();
-  }
-
+  renderSass();
   fs.watch(path.dirname("_sass/agency.scss"), renderSass);
-
 };
 
 function renderSass() {
   sass.render({file: "_sass/agency.scss"}, (err, result) => {
     if (err) return console.error(err);
-    fs.writeFile("_site/assets/css/agency.css", result.css.toString());
+    fs.writeFile("assets/css/agency.css", result.css.toString());
   })
 }
