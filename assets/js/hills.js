750acbdc243c040ae2c40e9f7c617856717ac89c
Tony DiPasquale <anthony.r.dipasquale@gmail.com>
2021-02-01 10:43:31 -0800

    Add team leaderboard

--- assets/js/hills.js
+++ assets/js/hills.js
@@ -1,31 +1,28 @@
+var map;
+
 $(function() {
-  const map = L.map('hill-map').setView([37.77, -122.44], 12);
+  map = L.map('hill-map').setView([37.77, -122.44], 12);
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     minZoom: 10,
     attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
   }).addTo(map);
 
+  if (localStorage.hills) {
+    addHillMarkers(JSON.parse(localStorage.hills));
+  }
+
   $.ajax({
     url: 'https://script.google.com/macros/s/AKfycbywSwe_b9zM5_V9hdlf6lPYXUwgjZyhjtMKBdrKIh4Fkf-NYhSD/exec?item=hills',
     dataType: 'json',
     cache: false,
     success: function (res) {
-      const hillTable = $('#hill-table tbody');
-      res.data.forEach( function(hill) {
-        if (hill.lat && hill.lng) {
-          $('#hill').append('<option value="'+hill.name+'">'+hill.name+'</option>');
-
-          var marker = L.marker([hill.lat, hill.lng]).addTo(map);
-          marker.bindPopup("<b>"+hill.name+"</b><br>"+hill.description+"<br>Length: "+hill.length+"mi Vert: "+hill.vert+"ft");
-          hillTable.append("<tr><td>"+hill.name+"</td><td colspan=\"3\">"+hill.description+"</td><td class=\"text-right\">"+hill.length+"</td><td class=\"text-right\">"+hill.vert+"</td></tr>");
-        }
-      });
+      localStorage.hills = JSON.stringify(res.data);
     },
     error: function (req, status, e) {
       console.error(e);
     }
-  });
+});
 
   $.ajax({
     url: '/assets/hills.json',
@@ -39,3 +36,16 @@ $(function() {
     }
   });
 });
+
+function addHillMarkers(hills) {
+  const hillTable = $('#hill-table tbody');
+  hills.forEach( function(hill) {
+    if (hill.lat && hill.lng) {
+      $('#hill').append('<option value="'+hill.name+'">'+hill.name+'</option>');
+
+      var marker = L.marker([hill.lat, hill.lng]).addTo(map);
+      marker.bindPopup("<b>"+hill.name+"</b><br>"+hill.description+"<br>Length: "+hill.length+"mi Vert: "+hill.vert+"ft");
+      hillTable.append("<tr><td>"+hill.name+"</td><td colspan=\"3\">"+hill.description+"</td><td class=\"text-right\">"+hill.length+"</td><td class=\"text-right\">"+hill.vert+"</td></tr>");
+    }
+  });
+}
--- assets/js/leaderboard.js
+++ assets/js/leaderboard.js
@@ -1,82 +1,99 @@
 $(function() {
-  return;
-  $.ajax({
-    url: 'https://script.google.com/macros/s/AKfycbywSwe_b9zM5_V9hdlf6lPYXUwgjZyhjtMKBdrKIh4Fkf-NYhSD/exec?item=results',
-    dataType: 'json',
-    cache: false,
-    success: function (res) {
-      const results = res.data;
+  const hillsPromise = $.Deferred();
 
-      const groupedRoutes = _.groupBy(results, 'route');
-      const groupedRoutesGender = _.mapValues(groupedRoutes, function (v) {
-        return _.groupBy(v, 'gender');
-      });
-
-      $('#leaderboard tbody').html(Object.keys(groupedRoutesGender).map( function (route) {
-        return Object.keys(groupedRoutesGender[route]).map( function (gender) {
-          const times = _.sortBy(groupedRoutesGender[route][gender], 'time');
-          const person = times[0];
-
-          const aOpen = '<a href="'+person.link+'" rel="noopener noreferrer" target="_blank" style="display: block;" class="text-reset">';
-          const aClose = "</a>"
-
-          return "<tr><td>"+aOpen+route+aClose+"</td><td>"+aOpen+_.upperFirst(gender)+aClose+"</td><td>"+aOpen+person.name+aClose+"</td><td>"+aOpen+person.team+aClose+"</td><td>"+aOpen+person.time+aClose+"</td></tr>";
-        }).join();
-      }));
-
-      if ($('#attempts').length) {
-        const uniqueAttempts = _.countBy(_.uniqWith(results, function (lhv, rhv) {
-          return lhv.id === rhv.id && lhv.route === rhv.route;
-        }), 'team');
-
-        $('#attempts tbody').html(Object.keys(uniqueAttempts).map( function (team) {
-          const records = Object.keys(groupedRoutesGender).reduce( function (accum, route) {
-            return accum + Object.keys(groupedRoutesGender[route]).reduce( function (accum, gender) {
-              return accum + (_.sortBy(groupedRoutesGender[route][gender], 'time')[0].team === team ? 1 : 0);
-            }, 0);
-          }, 0);
-          return "<tr><td>"+team+"</td><td class=\"text-center\">"+uniqueAttempts[team]+"</td><td class=\"text-right\">"+records+"</td></tr>";
-        }));
-      }
-
-      if ($('#grandslam').length) {
-        const groupedPeople = _.mapValues(_.groupBy(results, 'name'), function (v) {
-          return _.uniqBy(v, 'route');
-        });
-        const people = _.reverse(_.sortBy(Object.keys(groupedPeople).map(function (p) {
-          return {
-            name: p,
-            team: groupedPeople[p][0].team,
-            routesCompleted: groupedPeople[p].length,
-            routes: groupedPeople[p].map(function (i) { return i.route; }).join(", ")
-          };
-        }), 'routesCompleted'));
-
-        $('#grandslam tbody').html(people.map( function (entry) {
-          return "<tr><td>"+entry.name+"</td><td class=\"text-center\">"+entry.team+"</td><td class=\"text-right\">"+entry.routesCompleted+"</td></tr>";
-        }));
-      }
-
-      if ($('#route-results').length) {
-        const route = $('#route-results').data().route;
-        const top10Male = _.take(_.sortBy(_.filter(results, { 'route': route, 'gender': 'male' }), 'time'), 10);
-        const top10Female = _.take(_.sortBy(_.filter(results, { 'route': route, 'gender': 'female' }), 'time'), 10);
-        $('#route-results #female tbody').html(top10Female.map( function (entry) {
-          const aOpen = '<a href="'+entry.link+'" rel="noopener noreferrer" target="_blank" style="display: block;" class="text-reset">';
-          const aClose = "</a>"
-          return "<tr><td>"+aOpen+entry.name+aClose+"</td><td class=\"text-center\">"+aOpen+entry.team+aClose+"</td><td class=\"text-right\">"+aOpen+entry.time+aClose+"</td></tr>";
-        }));
-
-        $('#route-results #male tbody').html(top10Male.map( function (entry) {
-          const aOpen = '<a href="'+entry.link+'" rel="noopener noreferrer" target="_blank" style="display: block;" class="text-reset">';
-          const aClose = "</a>"
-          return "<tr><td>"+aOpen+entry.name+aClose+"</td><td class=\"text-center\">"+aOpen+entry.team+aClose+"</td><td class=\"text-right\">"+aOpen+entry.time+aClose+"</td></tr>";
-        }));
-      }
-
-    },
-    error: function (req, status, e) {
-      console.error(e);
+  const rejectHills = setTimeout(function () { hillsPromise.reject(); }, 10000);
+  function checkForHills() {
+    if (hillsPromise.state() === 'rejected') return;
+    if (localStorage.hills) {
+      clearTimeout(rejectHills);
+      hillsPromise.resolve(localStorage.hills);
+    } else {
+      setTimeout(checkForHills, 1000);
     }
+  }
+  checkForHills();
+
+  $.when(
+    $.ajax({
+      url: 'https://script.google.com/macros/s/AKfycbywSwe_b9zM5_V9hdlf6lPYXUwgjZyhjtMKBdrKIh4Fkf-NYhSD/exec?item=results',
+      dataType: 'json',
+      cache: false,
+    }),
+    $.ajax({
+      url: 'https://script.google.com/macros/s/AKfycbywSwe_b9zM5_V9hdlf6lPYXUwgjZyhjtMKBdrKIh4Fkf-NYhSD/exec?item=teams',
+      dataType: 'json',
+      cache: false,
+    }),
+    hillsPromise.promise()
+  ).then( function (resResults, resTeams, resHills) {
+    const results = resResults[0].data;
+    const teams = resTeams[0].data;
+    const hills = JSON.parse(resHills);
+
+    const resultsWithTeams = _.mapValues(results, function (r) {
+      r.team = _.find(teams, { id: r.id }).team;
+      return r;
+    });
+
+    const teamResults = _.groupBy(resultsWithTeams, 'team');
+    const teamReps = _.mapValues(teamResults, function (team) {
+      return _.reduce(team, function (accum, v) { return accum + v.reps; }, 0);
+    });
+    const teamVert = _.mapValues(teamResults, function (team) {
+      return _.reduce(team, function (accum, v) {
+        const vert = _.find(hills, { name: v.hill }).vert;
+        return accum + v.reps * vert;
+      }, 0);
+    });
+
+    const teamLabels = Object.keys(teamVert);
+    const teamValues = _.map(teamLabels, function (l) { return teamVert[l]; });
+    const ctx = $('#team-leaderboard');
+    const chart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: teamLabels,
+        datasets: [{
+          lable: '',
+          data: teamValues,
+          fill: false,
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+          ],
+          borderColor: [
+            "rgb(255, 99, 132)",
+            "rgb(75, 192, 192)",
+            "rgb(54, 162, 235)",
+            "rgb(153, 102, 255)",
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        maintainAspectRatio: false,
+        legend: {
+          display: false
+        },
+        scales: {
+          yAxes: [{
+            scaleLabel: {
+              display: true,
+              labelString: 'Vert (ft)'
+            },
+            ticks: {
+              beginAtZero: true,
+              callback: function(value, index, values) {
+                return value >= 1000 ? (value/1000).toFixed(2) + 'k' : value;
+              }
+            }
+          }]
+        }
+      }
+    });
+  }, function (req, status, e) {
+    console.error(e || req || 'Unknown Error Occurred');
   });
 });
